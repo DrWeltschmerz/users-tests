0187aa90382cb1349c60476b6994c93af1c88617
Dr Weltschmerz <23312554+DrWeltschmerz@users.noreply.github.com>
2025-09-15 03:42:08 +0200

    added reporting

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   testDir: "./",
   timeout: 30000,
   retries: 0,
+  reporter: [["html", { outputFolder: "playwright-report", open: "never" }]],
   use: {
     baseURL: "http://localhost:8080",
     trace: "on-first-retry",
